89fdc1d9a9a15af49e2b14c06c2a250ed3f7eb43
Ser9i02021 <78425749+Ser9i02021@users.noreply.github.com>
2024-05-29 02:52:27 -0300

    Update webScraping.py

--- webScraping.py
+++ webScraping.py
@@ -7,6 +7,8 @@ from selenium.webdriver.chrome.options import Options
 import json
 from time import sleep
 import re
+from datetime import datetime, timedelta
+import os
 
 # Ensure the path to ChromeDriver is correct
 chrome_driver_path = "/Users/pc/Downloads/TEGD-main/chromedriver.exe"
@@ -18,12 +20,67 @@ options.add_argument("--disable-blink-features=AutomationControlled")
 service = Service(executable_path=chrome_driver_path)
 driver = webdriver.Chrome(service=service, options=options)
 
-# URL of the page
-url = 'https://www.metoffice.gov.uk/weather/forecast/6gj7ng0qu#?date=2024-04-25'
+# Define the base URLs for the new cities
+new_cities = {
+    'London': 'https://www.metoffice.gov.uk/weather/forecast/gcpvj0v07#?date=2024-05-29',
+    'Bath': 'https://www.metoffice.gov.uk/weather/forecast/gcnk62de6#?date=2024-05-29',
+    'Bradford': 'https://www.metoffice.gov.uk/weather/forecast/gcwdpcpzj',
+    'Brighton and Hove': 'https://www.metoffice.gov.uk/weather/forecast/gcpcq5541',
+    'Birmingham': 'https://www.metoffice.gov.uk/weather/forecast/gcqdt4b2x',
+    'Bristol': 'https://www.metoffice.gov.uk/weather/forecast/gcnhtnumz',
+    'Cambridge': 'https://www.metoffice.gov.uk/weather/forecast/u1214b469',
+    'Canterbury': 'https://www.metoffice.gov.uk/weather/forecast/u10g8x4vg',
+    'Carlisle': 'https://www.metoffice.gov.uk/weather/forecast/gcvbs84rv#?date=2024-05-29',
+    'Chelmsford': 'https://www.metoffice.gov.uk/weather/forecast/u10q6cgzm#?date=2024-05-29',
+    'Chester': 'https://www.metoffice.gov.uk/weather/forecast/gcmyw5w26#?date=2024-05-29',
+    'Chichester': 'https://www.metoffice.gov.uk/weather/forecast/gcp3nqsgd#?date=2024-05-29',
+    'Coventry': 'https://www.metoffice.gov.uk/weather/forecast/gcqfjkq3z#?date=2024-05-29',
+    'Derby': 'https://www.metoffice.gov.uk/weather/forecast/gcqvn6pq4#?date=2024-05-29',
+    'Doncaster': 'https://www.metoffice.gov.uk/weather/forecast/gcx0qr7rt#?date=2024-05-29',
+    'Durham': 'https://www.metoffice.gov.uk/weather/forecast/gcwzefp2c#?date=2024-05-29',
+    'Ely': 'https://www.metoffice.gov.uk/weather/forecast/gcjsyk4mw#?nearestTo=Ely%20(Cardiff)&date=2024-05-29',
+    'Exeter': 'https://www.metoffice.gov.uk/weather/forecast/gcj2x8gt4#?date=2024-05-29',
+    'Gloucester': 'https://www.metoffice.gov.uk/weather/forecast/gcnrj1e0w#?date=2024-05-29',
+    'Hereford': 'https://www.metoffice.gov.uk/weather/forecast/gcq04hx21#?date=2024-05-29',
+    'Kingston upon Hull': 'https://www.metoffice.gov.uk/weather/forecast/gcxcb25c4#?date=2024-05-29',
+    'Lancaster': 'https://www.metoffice.gov.uk/weather/forecast/gcw52qce5#?date=2024-05-29',
+    'Leeds': 'https://www.metoffice.gov.uk/weather/forecast/gcwfhf1w0#?date=2024-05-29',
+    'Leicester': 'https://www.metoffice.gov.uk/weather/forecast/gcr5qn5jy#?date=2024-05-29',
+    'Lichfield': 'https://www.metoffice.gov.uk/weather/forecast/gcqewq76c#?date=2024-05-29',
+    'Lincoln': 'https://www.metoffice.gov.uk/weather/forecast/gcrwgdr98#?date=2024-05-29',
+    'Liverpool': 'https://www.metoffice.gov.uk/weather/forecast/gcmzggpxq#?date=2024-05-29',
+    'Manchester': 'https://www.metoffice.gov.uk/weather/forecast/gcw2hzs1u#?date=2024-05-29',
+    'Milton Keynes': 'https://www.metoffice.gov.uk/weather/forecast/gcr2nc3b7#?date=2024-05-29',
+    'Newcastle upon Tyne': 'https://www.metoffice.gov.uk/weather/forecast/gcybg0rne#?date=2024-05-29',
+    'Norwich': 'https://www.metoffice.gov.uk/weather/forecast/u12gmt1fz#?date=2024-05-29',
+    'Nottingham': 'https://www.metoffice.gov.uk/weather/forecast/gcrjp3v96#?date=2024-05-29',
+    'Oxford': 'https://www.metoffice.gov.uk/weather/forecast/gcpn7mp10#?date=2024-05-29',
+    'Peterborough': 'https://www.metoffice.gov.uk/weather/forecast/gcrg49fhe#?date=2024-05-29',
+    'Plymouth': 'https://www.metoffice.gov.uk/weather/forecast/gbvn9cv4h#?date=2024-05-29',
+    'Portsmouth': 'https://www.metoffice.gov.uk/weather/forecast/gcp0zn6wn#?date=2024-05-29',
+    'Preston': 'https://www.metoffice.gov.uk/weather/forecast/gcw1fe28j#?date=2024-05-29',
+    'Ripon': 'https://www.metoffice.gov.uk/weather/forecast/gcwgvr0fn#?date=2024-05-29',
+    'Salford': 'https://www.metoffice.gov.uk/weather/forecast/gcw279prq#?date=2024-05-29',
+    'Salisbury': 'https://www.metoffice.gov.uk/weather/forecast/gcndx0wq3#?date=2024-05-29',
+    'Sheffield': 'https://www.metoffice.gov.uk/weather/forecast/gcqzwtdw7#?date=2024-05-29',
+    'Southampton': 'https://www.metoffice.gov.uk/weather/forecast/gcp185f25#?date=2024-05-29',
+    'Southend-on-Sea': 'https://www.metoffice.gov.uk/weather/forecast/u10t0nxqf#?date=2024-05-29',
+    'St Albans': 'https://www.metoffice.gov.uk/weather/forecast/gcpy2m1yy#?date=2024-05-29',
+    'Stoke-on-Trent': 'https://www.metoffice.gov.uk/weather/forecast/gcqmw2y12#?date=2024-05-29',
+    'Sunderland': 'https://www.metoffice.gov.uk/weather/forecast/gcz02e3x2#?date=2024-05-29',
+    'Truro': 'https://www.metoffice.gov.uk/weather/forecast/gbumvn49q#?date=2024-05-29',
+    'Wakefield': 'https://www.metoffice.gov.uk/weather/forecast/gcwcmu8w8#?date=2024-05-29',
+    'Wells': 'https://www.metoffice.gov.uk/weather/forecast/gcn57f71q#?date=2024-05-29',
+    'Westminster': 'https://www.metoffice.gov.uk/weather/forecast/gcpuuyzwv#?date=2024-05-29',
+    'Winchester': 'https://www.metoffice.gov.uk/weather/forecast/gcp46pp1c#?date=2024-05-29',
+    'Wolverhampton': 'https://www.metoffice.gov.uk/weather/forecast/gcq7pt4g5#?date=2024-05-29',
+    'Worcester': 'https://www.metoffice.gov.uk/weather/forecast/gcq2vmx21#?date=2024-05-29',
+    'York': 'https://www.metoffice.gov.uk/weather/forecast/gcx4zrw25#?date=2024-05-29'
+}
 
 # Main class to collect data
-class WeatherConditions():
-    def __init__(self) -> None:
+class WeatherConditions:
+    def __init__(self, url):
         driver.get(url)
         sleep(3)  # Wait for the page to load
         self.content = driver.page_source
@@ -63,11 +120,13 @@ class WeatherConditions():
         else:
             sunset_time = 'No sunset time found'
 
-        tab = self.soup.find('li', {'data-tab-index': str(day)})
         uv_elem = tab.find('span', {'data-type': 'uv'})
         if uv_elem:
             uv_label = uv_elem['data-category']
             uv_value = uv_elem['data-value']
+        else:
+            uv_label = 'No UV data found'
+            uv_value = 'No UV data found'
 
         return {
             "weather_summary": ws,
@@ -191,43 +250,52 @@ class WeatherConditions():
 
         return chance_of_rain_data
 
-def get_data_and_wrap_it_to_JSON():
-    weather_conditions = WeatherConditions()
-    # Collect data for 7 days
+def get_data_and_wrap_it_to_JSON(from_item: int, to_item: int):
     data = []
-    for day in range(7):
-        # Get the forecast date from the source code
-        date = weather_conditions.get_forecast_date(day)
 
-        summary = weather_conditions.summary_weather_conditions(day)  # Sunrise and sunset times + overall weather condition for a given day
-        min_max = weather_conditions.min_max_temps(day)  # Min and max temps for a given day
-        all_temps = weather_conditions.all_temps_for_day(day)  # All temps for a given day
-        all_chances_of_rain = weather_conditions.chance_of_precipitation(day)  # Chances of rain for a given day
+    # Load existing data if the JSON file exists
+    if os.path.exists("w_data.json"):
+        with open("w_data.json", "r", encoding="utf-8") as f:
+            data = json.load(f)
 
-        # Combine temperatures and chances of rain
-        all_temps_and_chances_of_rain = {}
-        for time in all_temps.keys():
-            all_temps_and_chances_of_rain[time] = {
-                "temperature": all_temps[time],
-                "chance_of_rain": all_chances_of_rain.get(time, "No data")
-            }
+    for city, url in dict(list(new_cities.items())[from_item:to_item]).items():
+        weather_conditions = WeatherConditions(url)
+        # Collect data for 7 days
+        for day in range(7):
+            # Get the forecast date from the source code
+            date = weather_conditions.get_forecast_date(day)
 
-        # Append the data for each day as a separate entry in the JSON list
-        data.append({
-            "day": date,
-            "summary": summary,
-            "min_max": min_max,
-            "all_temps_and_chances_of_rain": all_temps_and_chances_of_rain
-        })
+            summary = weather_conditions.summary_weather_conditions(day)  # Sunrise and sunset times + overall weather condition for a given day
+            min_max = weather_conditions.min_max_temps(day)  # Min and max temps for a given day
+            all_temps = weather_conditions.all_temps_for_day(day)  # All temps for a given day
+            all_chances_of_rain = weather_conditions.chance_of_precipitation(day)  # Chances of rain for a given day
+
+            # Combine temperatures and chances of rain
+            all_temps_and_chances_of_rain = {}
+            for time in all_temps.keys():
+                all_temps_and_chances_of_rain[time] = {
+                    "temperature": all_temps[time],
+                    "chance_of_rain": all_chances_of_rain.get(time, "No data")
+                }
+
+            # Append the data for each day as a separate entry in the JSON list
+            data.append({
+                "city": city,
+                "day": date,
+                "summary": summary,
+                "min_max": min_max,
+                "all_temps_and_chances_of_rain": all_temps_and_chances_of_rain
+            })
 
     # Save the collected data to a JSON file
-    with open("weather_data.json", "w", encoding="utf-8") as f:
+    with open("w_data.json", "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
     print("Weather data collected and saved successfully!")
 
 # Collect and save weather data to JSON
-get_data_and_wrap_it_to_JSON()
+for i in range(10, 53, 2):
+    get_data_and_wrap_it_to_JSON(i, i + 2)
 
 # Close the driver
 driver.quit()
